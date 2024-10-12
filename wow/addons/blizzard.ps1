448838b1dd92e4e0c1444bab6da9b805f5419f0f
pedrozc90 <pedrozc90@gmail.com>
2024-10-12 10:34:43 -0300

    update blizzard.ps1

--- wow/addons/blizzard.ps1
+++ wow/addons/blizzard.ps1
@@ -17,12 +17,27 @@ if (Test-Path $RETAIL_DIR) {
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tainted" -Target "$TAINTED_DIR" -Force
 }
 
+$PTR_DIR = "$WOW_DIR\_ptr_"
+if (Test-Path $PTR_DIR) {
+    $ADDONS_DIR = "$PTR_DIR\Interface\AddOns"
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Filger" -Target "$FILGER_DIR" -Force
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tainted" -Target "$TAINTED_DIR" -Force
+}
+
+$XPTR_DIR = "$WOW_DIR\_xptr_"
+if (Test-Path $XPTR_DIR) {
+    $ADDONS_DIR = "$XPTR_DIR\Interface\AddOns"
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Filger" -Target "$FILGER_DIR" -Force
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tainted" -Target "$TAINTED_DIR" -Force
+}
+
 $CLASSIC_ERA_DIR = "$WOW_DIR\_classic_era_"
 if (Test-Path $CLASSIC_ERA_DIR) {
     $ADDONS_DIR = "$CLASSIC_ERA_DIR\Interface\AddOns"
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tukui" -Target "$TUKUI_DIR" -Force
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\LuaUI" -Target "$LUAUI_DIR" -Force
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Filger" -Target "$FILGER_DIR" -Force
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tainted" -Target "$TAINTED_DIR" -Force
 }
 
 $CLASSIC_DIR = "$WOW_DIR\_classic_"
@@ -31,4 +46,11 @@ if (Test-Path $CLASSIC_DIR) {
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tukui" -Target "$TUKUI_DIR" -Force
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\LuaUI" -Target "$LUAUI_DIR" -Force
     New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Filger" -Target "$FILGER_DIR" -Force
+}
+
+$CLASSIC_PTR_DIR = "$WOW_DIR\_classic_ptr_"
+if (Test-Path $CLASSIC_PTR_DIR) {
+    $ADDONS_DIR = "$CLASSIC_PTR_DIR\Interface\AddOns"
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Filger" -Target "$FILGER_DIR" -Force
+    New-Item -ItemType SymbolicLink -Path "$ADDONS_DIR\Tainted" -Target "$TAINTED_DIR" -Force
 }
